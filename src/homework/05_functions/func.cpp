836a6feb9a0328969334d15f68e9cbb17f88a8bd
brycelikesbands <157327978+brycelikesbands@users.noreply.github.com>
2024-03-03 00:01:50 +0000

    Homework 5

--- src/homework/05_functions/func.cpp
+++ src/homework/05_functions/func.cpp
@@ -1,3 +1,44 @@
 //add include statements
-
+#include "func.h"
 //add function code here
+#include "func.h"
+
+double get_gc_content(const std::string& dna) {
+    int gc_count = 0;
+    for (char nucleotide : dna) {
+        if (nucleotide == 'G' || nucleotide == 'C') {
+            gc_count++;
+        }
+    }
+    return static_cast<double>(gc_count) / dna.size();
+}
+
+std::string get_reverse_string(std::string dna) {
+    for (int i = 0, j = dna.size() - 1; i < j; i++, j--) {
+        std::swap(dna[i], dna[j]);
+    }
+    return dna;
+}
+
+std::string get_dna_complement(std::string dna) {
+    get_reverse_string(dna); // Step a: Reverse the string
+    for (char& nucleotide : dna) {
+        switch (nucleotide) {
+            case 'A':
+                nucleotide = 'T';
+                break;
+            case 'T':
+                nucleotide = 'A';
+                break;
+            case 'C':
+                nucleotide = 'G';
+                break;
+            case 'G':
+                nucleotide = 'C';
+                break;
+            default:
+                break;
+        }
+    }
+    return dna;
+}
--- src/homework/05_functions/func.h
+++ src/homework/05_functions/func.h
@@ -1,1 +1,11 @@
 //add function(s) prototype here
+#ifndef FUNC_H
+#define FUNC_H
+
+#include <string>
+
+double get_gc_content(const std::string& dna);
+std::string get_reverse_string(std::string dna);
+std::string get_dna_complement(std::string dna);
+
+#endif
